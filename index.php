945541b9e125fbd8bdeed0cf25b85512302eff3a
UnRekt <server@unrekt.at>
2018-09-28 16:07:06 +0200

    Base

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+<title>Tha Gang - ZT</title>
+	
+<meta charset="utf-8">
+	
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/mobile/style.css">
+</head>
+
+<body>
+</body>
+</html>
