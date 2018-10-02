8a05bb1c91b25220583e99f84dadf546c2a78058
UnRekt <server@unrekt.at>
2018-10-02 21:17:16 +0200

    Background

--- index.php
+++ index.php
@@ -4,11 +4,14 @@
 <title>Tha Gang - ZT</title>
 	
 <meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	
+<link href="https://fonts.googleapis.com/css?family=Bai+Jamjuree" rel="stylesheet">
 <link rel="stylesheet" href="css/style.css">
 <link rel="stylesheet" href="css/mobile/style.css">
 </head>
 
 <body>
+	<h1 class="logoName">Tha Gang ZT</h1>
 </body>
 </html>
